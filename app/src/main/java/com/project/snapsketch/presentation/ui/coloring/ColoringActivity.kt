a36815569ecaa5ae2c54beccdc7c5d22dd6638ee
neoneoneo123 <gouxrunner@gmail.com>
2024-08-05 20:24:47 +0900

    add: 화면 추가 - home - coloring - setting

--- app/src/main/java/com/project/snapsketch/presentation/ui/coloring/ColoringActivity.kt
+++ app/src/main/java/com/project/snapsketch/presentation/ui/coloring/ColoringActivity.kt
@@ -1,4 +1,4 @@
-package com.project.snapsketch.presentation.ui
+package com.project.snapsketch.presentation.ui.coloring
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -6,14 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.project.snapsketch.R
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class ColoringActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coloring)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
