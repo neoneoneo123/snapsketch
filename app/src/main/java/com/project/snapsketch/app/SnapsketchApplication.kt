d1add4fe2fce802683031966e9ea122bc5208d9c
neoneoneo123 <gouxrunner@gmail.com>
2024-08-05 20:18:04 +0900

    add: 최초 설정 - 종속성 추가 - 패키지 구조 설정

---
+++ app/src/main/java/com/project/snapsketch/app/SnapsketchApplication.kt
@@ -0,0 +1,8 @@
+package com.project.snapsketch.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SnapsketchApplication: Application() {
+}
--- app/src/main/java/com/project/snapsketch/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/project/snapsketch/presentation/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.project.snapsketch
+package com.project.snapsketch.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.project.snapsketch.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
